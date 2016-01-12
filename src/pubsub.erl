c3a25cde7df744a412a031cc0624520c08425fe6
Sebastian Green-Husted <ricecake@tfm.nu>
2016-01-11 19:02:05 -0500

    make interface more backwards compatible

--- src/pubsub.erl
+++ src/pubsub.erl
@@ -3,7 +3,9 @@
 -export([
 	start/0,
 	declare_exchange/1,
+	publish/2,
 	publish/3,
+	subscribe/1,
 	subscribe/2,
 	subscribe/3,
 	unsubscribe/2,
@@ -15,9 +17,14 @@ start() ->
 
 declare_exchange(Exchange) -> pubsub_srv:declare_exchange(Exchange).
 
+publish(Topic, Message) -> publish(default, Topic, Message).
+
 publish(Exchange, Topic, Message) -> pubsub_srv:publish(Exchange, Topic, Message).
 
-subscribe(Exchange, Topic) when is_atom(Exchange) -> pubsub_srv:subscribe(Exchange, Topic).
+subscribe(Topic) -> subscribe(default, Topic).
+
+subscribe(Exchange, Topic) when is_atom(Exchange) -> pubsub_srv:subscribe(Exchange, Topic);
+subscribe(Topic, Callback) when is_function(Callback) -> subscribe(default, Topic, Callback).
 
 subscribe(Exchange, Topic, Callback) -> pubsub_srv:subscribe(Exchange, Topic, Callback).
 
